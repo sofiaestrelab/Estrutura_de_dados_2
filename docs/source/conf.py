81116bcbec583f4cc1b9919fb4703eaa65636c67
Sofia Estrela Bernardes <130997583+sofiaestrelab@users.noreply.github.com>
2026-09-07 18:38:42 -0300

    Revise copyright and author details
    Updated copyright and author information in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # Informações projeto
 project = 'Estrutura de Dados 2'
-copyright = '2026, Ciências da Computação, Sofia Estrela Bernardes, Alisson dos Reis'
-author = 'Sofia Estrela e Alisson dos Reis'
+copyright = '2026, Ciências da Computação, Sofia Estrela Bernardes'
+author = 'Sofia Estrela Bernardes'
 
 release = '2.0'
 version = '2.0'
